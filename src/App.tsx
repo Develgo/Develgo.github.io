a6fcd1cd0149319aeb71ee7530b1a69e4dede5f8
Nitesh Maharaj <nitesh.maharaj@entelect.co.za>
2023-04-18 09:04:44 +0200

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import NavBar from './components/NavBar';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import CareersPage from './pages/CareersPage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/about" element={<AboutPage />}></Route>
+        <Route path="/careers" element={<CareersPage />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+    return (
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/careers">Careers</Link>
+        </div>
+    );
+};
+
+export default NavBar;
